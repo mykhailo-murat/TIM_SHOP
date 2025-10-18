0ab1775ed4ed4b80658b9e527997be22039eba7f
mykhailo-murat <mykhailo.murat@gmail.com>
2025-10-18 17:45:37 +0300

    + urls

--- TIM_SHOP/urls.py
+++ TIM_SHOP/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('users/', include('users.urls', namespace='users')),
     path('', include('main.urls', namespace='main')),
 ]
 
